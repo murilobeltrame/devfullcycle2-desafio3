b48cb1eafcc03d4e5e3574d497423254ba330d22
Murilo Beltrame <murilobeltrame@gmail.com>
2020-05-08 23:54:26 -0300

    added static serving

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SenderController } from './controllers/sender/sender.controller';
+import { ReceiverController } from './controllers/receiver/receiver.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, SenderController, ReceiverController],
   providers: [AppService],
 })
 export class AppModule {}
